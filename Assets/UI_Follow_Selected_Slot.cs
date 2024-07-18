f4d7c997eef490b9f33f66766846f9831e0d7996
DavidUllauri <ullauridavid@gmail.com>
2024-07-17 20:08:20 -0400

    Save Game part 4, ep-12

---
+++ Assets/UI_Follow_Selected_Slot.cs
@@ -0,0 +1,44 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
+
+namespace DU
+{
+    public class UI_Follow_Selected_Slot : MonoBehaviour
+    {
+        [SerializeField] GameObject currentSelected;
+        [SerializeField] GameObject previouslySelected;
+        [SerializeField] RectTransform currentSelectedTransform;
+
+        [SerializeField] RectTransform contentPanel;
+        [SerializeField] ScrollRect scrollRect;
+
+        private void Update()
+        {
+            currentSelected = EventSystem.current.currentSelectedGameObject;
+
+            if (currentSelected != null )
+            {
+                if (currentSelected != previouslySelected)
+                {
+                    previouslySelected = currentSelected;
+                    currentSelectedTransform = currentSelected.GetComponent<RectTransform>();
+                    SnapTo(currentSelectedTransform);
+                }
+            }
+        }
+
+        private void SnapTo(RectTransform target)
+        {
+            Canvas.ForceUpdateCanvases();
+
+            Vector2 newPosition = (Vector2)scrollRect.transform.InverseTransformPoint(contentPanel.position) - (Vector2)scrollRect.transform.InverseTransformPoint(target.position);
+
+            newPosition.x = 0;
+
+            contentPanel.anchoredPosition = newPosition;
+        }
+    }
+}
