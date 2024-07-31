938dc8a7848456189dffe803772b630836abc987
DavidUllauri <ullauridavid@gmail.com>
2024-07-31 19:12:58 -0400

    Character Effects part 1, ep-15

---
+++ Assets/Scripts/Character/CharacterEffectsManager.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace DU
+{
+    public class CharacterEffectsManager : MonoBehaviour
+    {
+        CharacterManager character;
+
+        protected virtual void Awake()
+        {
+            character = GetComponent<CharacterManager>();
+        }
+
+        // Process Instant Effects
+        public virtual void ProcessInstantEffects(InstantCharacterEffect effect)
+        {
+            effect.ProcessEffect(character);
+        }
+
+        // Process Timed Effects
+
+        // Process static effects
+    }
+}
---
+++ Assets/WorldCharacterEffectsManager.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace DU
+{
+    public class WorldCharacterEffectsManager : MonoBehaviour
+    {
+        public static WorldCharacterEffectsManager Instance;
+
+        [SerializeField] List<InstantCharacterEffect> instantEffects;
+
+        private void Awake()
+        {
+            if (Instance == null)
+            {
+                Instance = this;
+            }
+            else
+            {
+                Destroy(gameObject);
+            }
+        }
+
+        private void GenerateEffectIDs()
+        {
+            for (int i = 0; i < instantEffects.Count; ++i)
+            {
+                instantEffects[i].instantEffectID = i;
+            }
+        }
+    }
+}
+
